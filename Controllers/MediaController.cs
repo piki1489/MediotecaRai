dd187ca1bdcaa3bb2970e0f54f469279c499f62e
piki1 <piki1489@hotmail.com>
2026-01-22 22:16:48 +0100

    Busqueda de la parte pública completa

---
+++ Controllers/MediaController.cs
@@ -0,0 +1,31 @@
+﻿using MediotecaRai.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediotecaRai.Controllers
+{
+    public class MediaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MediaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string tipo, string titulo)
+        {
+            var query = _context.MediaItems.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(tipo))
+            {
+                query = query.Where(m => m.Tipo == tipo); 
+            }
+            if (!string.IsNullOrWhiteSpace(titulo))
+            {
+                query = query.Where(m => m.Titulo == titulo);
+            }
+            var resultados = query.ToList();
+            return View(resultados);
+        }
+    }
+}
