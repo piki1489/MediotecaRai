f9450d26e71e757931e553505298064fc4ad070e
piki1 <piki1@PIKI1489>
2026-01-22 20:00:06 +0100

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediotecaRai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
